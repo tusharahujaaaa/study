47f5952eaa587e8e1845eb81f0f61051bdd821a5
Tushar <tushar.ahuja@netprohetsglobal.com>
2025-08-20 15:22:05 +0530

    m

--- index.js
+++ index.js
@@ -9,43 +9,63 @@
 * */
 
 
-/* 
-*    1 2 3 4
-*     2 3 4
-*      3 4
-*       4
-*      3 4
-*     2 3 4
-*    1 2 3 4
-* */
+// /*
+// *    1 2 3 4
+// *     2 3 4
+// *      3 4
+// *       4
+// *      3 4
+// *     2 3 4
+// *    1 2 3 4
+// * */
 
 
+
+
+// function draw(n) {
+//     let str = '';
+//     let map = new Map();
+//     for (let i = 0; i < 4; i++) {
+//         for (let j = 0; j < n / 2; j++) {
+//             if (j < i) {
+//                 str += ' ';
+//             } else {
+//                 str += (j + 1) + ' '; // leading number
+//             }
+//         }
+//         map.set(i, str);
+//         str = '';
+//     }
+//     // console.log(map);
+//     for (let i = 0; i < n - 1; i++) {
+//         if (i < n / 2) {
+//             str += map.get(i);
+//         } else {
+//             str += map.get(n-i-2);
+//         }
+//         str += '\n';
+//     }
+//     console.log(str.trim());
+// }
+
+
+// draw(8);
 
 
 function draw(n) {
     let str = '';
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n ; j++) {
-            if (j < i) {
+    for (let i = 0; i < 2 * n - 1; i++) {
+        let row = (i < n) ? i : 2 * n - 2 - i; // mirror row index
+        for (let j = 0; j < n; j++) {
+            if (j < row) {
                 str += ' ';
             } else {
-                str += (j + 1) + ' '; // leading number
+                str += (j + 1) + ' ';
             }
         }
         str += '\n';
     }
-    for (let i = n-1; i > 0; i--){
-        for (let j = 0; j<n; j++) {
-            if (j < i - 1) {
-                str += ' ';
-            } else {
-                str += (j + 1) + ' '; // leading number
-            }
-        }
-        str += '\n';
-    }
-    console.log(str.trim());
+    console.log(str.trimEnd());
 }
-
 
 draw(4);
--- pattern.js
+++ pattern.js
@@ -7,9 +7,6 @@
          * 
 */
 
-
-
-
 function draw(n) {
     let str = '';
     for (let i = 0; i < n; i++) {
@@ -37,8 +34,6 @@ draw(6);
 *      3 4
 *       4
 * */
-
-
 
 function draw(n) {
     let str = '';
@@ -71,9 +66,6 @@ draw(4);
 *    1 2 3 4
 * */
 
-
-
-
 function draw(n) {
     let str = '';
     for (let i = 0; i < n; i++) {
@@ -99,5 +91,74 @@ function draw(n) {
     console.log(str.trim());
 }
 
+
+draw(4);
+
+
+/* 
+*    1 2 3 4
+*     2 3 4
+*      3 4
+*       4
+*      3 4
+*     2 3 4
+*    1 2 3 4
+* */
+
+function draw(n) {
+    let str = '';
+    let map = new Map();
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < n / 2; j++) {
+            if (j < i) {
+                str += ' ';
+            } else {
+                str += (j + 1) + ' '; // leading number
+            }
+        }
+        map.set(i, str);
+        str = '';
+    }
+    // console.log(map);
+    for (let i = 0; i < n - 1; i++) {
+        if (i < n / 2) {
+            str += map.get(i);
+        } else {
+            str += map.get(n-i-2);
+        }
+        str += '\n';
+    }
+    console.log(str.trim());
+}
+
+
+draw(8);
+
+
+/* 
+*    1 2 3 4
+*     2 3 4
+*      3 4
+*       4
+*      3 4
+*     2 3 4
+*    1 2 3 4
+* */
+
+function draw(n) {
+    let str = '';
+    for (let i = 0; i < 2 * n - 1; i++) {
+        let row = (i < n) ? i : 2 * n - 2 - i; // mirror row index
+        for (let j = 0; j < n; j++) {
+            if (j < row) {
+                str += ' ';
+            } else {
+                str += (j + 1) + ' ';
+            }
+        }
+        str += '\n';
+    }
+    console.log(str.trimEnd());
+}
 
 draw(4);
