de7cd9015f51086eb9f8bd1ee3d5a65f202e9acf
Tushar <tushar.ahuja@netprohetsglobal.com>
2025-09-03 12:56:41 +0530

    number right angle triangle

--- pattern.js
+++ pattern.js
@@ -223,3 +223,30 @@ function drawPattern(n) {
 
 
 drawPattern(6);
+
+
+
+/*
+*    1
+*    1 2
+*    1 2 3
+*    1 2 3 4
+*    1 2 3 4 5
+*    1 2 3 4 5 6
+*/
+
+function drawPattern(n) {
+    let str = '';
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            str += j + ' '
+        }
+        str += '\n'
+    }
+    console.log(str);
+
+}
+
+
+
+drawPattern(6);
