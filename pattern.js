c2054586fd118195bc3ce7c5106eb75001f4feeb
Tushar <tushar.ahuja@netprohetsglobal.com>
2025-09-03 13:24:09 +0530

    groingrightangle triangle

--- pattern.js
+++ pattern.js
@@ -276,3 +276,32 @@ function drawPattern(n) {
 
 
 drawPattern(6);
+
+
+
+/*
+*    1
+*    2 3
+*    4 5 6
+*    7 8 9 10
+*    11 12 13 14 15
+*    16 17 18 19 20 21
+*/
+
+function drawPattern(n) {
+    let str = '';
+    let t = 1;
+    for (let i = 1; i <= n; i++) {
+
+        for (let j = 1; j <= i; j++) {
+            str += t++ + ' '
+        }
+        str += '\n'
+    }
+    console.log(str);
+
+}
+
+
+
+drawPattern(6);
