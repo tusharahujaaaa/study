c4748f49efd3d48ed358bc275e5ead9c6f673ac3
Tushar <tushar.ahuja@netprohetsglobal.com>
2025-09-03 15:12:14 +0530

    palindrome triangle

--- index.js
+++ index.js
@@ -9,19 +9,22 @@
 * */
 
 /*
-*    1 2 3 4 5 6 
-*    1 2 3 4 5 
-*    1 2 3 4 
-*    1 2 3 
-*    1 2 
 *    1
+*    0 1
+*    1 0 1
+*    0 1 0 1
+*    1 0 1 0 1
+*    0 1 0 1 0 1
 */
 
 function drawPattern(n) {
     let str = '';
     for (let i = 1; i <= n; i++) {
-        for (let j = i; j <=n ; j++) {
-            str += j + ' '
+        let t = i % 2 == 0 ? 0 : 1;
+        for (let j = 1; j <= i; j++) {
+            str += t + ' '
+            // str += (i + j) % 2 + ' '
+            t = t == 0 ? 1 : 0
         }
         str += '\n'
     }
--- pattern.js
+++ pattern.js
@@ -305,3 +305,33 @@ function drawPattern(n) {
 
 
 drawPattern(6);
+
+
+
+/*
+*    1
+*    0 1
+*    1 0 1
+*    0 1 0 1
+*    1 0 1 0 1
+*    0 1 0 1 0 1
+*/
+
+function drawPattern(n) {
+    let str = '';
+    for (let i = 1; i <= n; i++) {
+        let t = i % 2 == 0 ? 0 : 1;
+        for (let j = 1; j <= i; j++) {
+            str += t + ' '
+            // str += (i + j) % 2 + ' '
+            t = t == 0 ? 1 : 0
+        }
+        str += '\n'
+    }
+    console.log(str);
+
+}
+
+
+
+drawPattern(6);
